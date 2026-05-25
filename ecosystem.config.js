5b26d81f210354a82c7df44ae6591201020f9817
azizu1012 <133684312+azizu1012@users.noreply.github.com>
2026-05-25 20:44:28 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       name: "azuris-bot",
       cwd: projectRoot,
       script: path.join(projectRoot, "run_bot.py"),
-      args: ["--server"], // ÉP CỨNG CHẾ ĐỘ PRODUCTION: UP CẢ BOT CORE VÀ API GATEWAY SERVER
+      args: [], // ÉP CỨNG CHẾ ĐỘ PRODUCTION: UP CẢ BOT CORE VÀ API GATEWAY SERVER
       interpreter,
       watch: false,
       max_memory_restart: "1G",
